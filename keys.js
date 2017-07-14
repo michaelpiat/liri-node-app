16ae2d35d043d1488a69e1521bf986613006ea7e
Michael Piatnichko <mpiatnichko@gmail.com>
2017-07-14 15:11:46 -0600

    initial setup

---
+++ keys.js
@@ -0,0 +1,9 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: "EOEP1mDpGb24u0zwWFlI6rgTP",
+  consumer_secret: 	"8mCpS96vBj1iT2aodmv3GK1wq6f5dy4vNxqhasIOiLQj91sQpA",
+  access_token_key: "885965119406063624-JSS9ytq2dieHfk1c6OvZ9TJLQ7GGsip",
+  access_token_secret: 	"zyU7jvND7tC1E5jh2NBgxaOfobGAxDg5gLSufJdcF4kFu",
+
+}
---
+++ liri.js
@@ -0,0 +1,18 @@
+var keys = require("./bands.js");
+
+var command = process.argv[2]; 
+
+switch (command) {
+	case "my-tweets":
+		//functions goes here
+		break;
+	case "spotify-this-song":
+		//functions goes here
+		break;
+	case "movie-this":
+		//functions goes here
+		break;
+	case "do-what-it-says":
+		//functions goes here
+		break;
+}
